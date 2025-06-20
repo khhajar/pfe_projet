52f7b3d509409770b43b15e672bd3468b1f32279
hajar kharchich <hajarkharchich10@gmail.com>
2025-06-20 01:37:10 +0100

    locall firebase config

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-  apiKey: "AIzaSyDtovaS_TIr6iokUwYfMuVABkH7I7uQRx4",
-  authDomain: "jobportal-8e469.firebaseapp.com",
-  projectId: "jobportal-8e469",
-  storageBucket: "jobportal-8e469.firebasestorage.app",
-  messagingSenderId: "989362408603",
-  appId: "1:989362408603:web:26b3b1198f41413be08a3e"
-  },
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+},
   production: false,
 };
